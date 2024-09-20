6b193db5c918aa90df73e82acaa0a6ac41f99595
Joaco Balian <joaquin.balian@correo.ucu.edu.uy>
2024-09-20 18:05:46 -0300

    Casos prueba con errores

--- Compuertas.Tests/CompuertasTest.cs
+++ Compuertas.Tests/CompuertasTest.cs
@@ -16,28 +16,26 @@ namespace Compuertas.Tests
         [TestCase(true, true, true, false, false, true)]     // A=1, B=1, C=1
         public void TestTablaDeVerdad(bool a, bool b, bool c, bool NotA, bool And, bool Or)
         {
-            //  A negado
             var entradaA = new Entrada(a);
             var notGateA = new CompuertaNot();
             notGateA.AddEntrada(entradaA);
             var resultadoNotA = notGateA.CalcularSalida();
-            Assert.AreEqual(NotA, resultadoNotA, $"El resultado de !A debería ser {NotA}");
+            Assert.AreEqual(NotA, resultadoNotA, $"El resultado de A negado debería ser {NotA}");
 
-            // A negado y C
             var entradaC = new Entrada(c);
             var andGate = new CompuertaAnd();
             andGate.AddEntrada(notGateA); 
             andGate.AddEntrada(entradaC); 
             var resultadoAnd = andGate.CalcularSalida();
-            Assert.AreEqual(And, resultadoAnd, $"El resultado de !A && C debería ser {And}");
+            Assert.AreEqual(And, resultadoAnd, $"El resultado de A negado AND C debería ser {And}");
 
-            // Compuerta OR ((!A && C) || B)
+            
             var entradaB = new Entrada(b);
             var orGate = new CompuertaOr();
-            orGate.AddEntrada(andGate); // !A && C
-            orGate.AddEntrada(entradaB); // B
+            orGate.AddEntrada(andGate); 
+            orGate.AddEntrada(entradaB); 
             var resultadoOr = orGate.CalcularSalida();
-            Assert.AreEqual(Or, resultadoOr, $"El resultado de (!A && C) || B debería ser {Or}");
+            Assert.AreEqual(Or, resultadoOr, $"El resultado de A negado AND C || B debería ser {Or}");
         }
     }
 }
